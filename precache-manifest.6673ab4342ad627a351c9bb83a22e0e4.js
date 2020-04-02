5b3af139e090303b55492d1f72ce6d6e33cf80e9
Brian Zinn <github@wakeskate.com>
2020-04-02 13:46:55 -0700

    Updates

---
+++ precache-manifest.6673ab4342ad627a351c9bb83a22e0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f5e1c7b35ec19d99809a06ce897887",
+    "url": "/post-process-a11y/index.html"
+  },
+  {
+    "revision": "6e76ae9ca86acbe23ec0",
+    "url": "/post-process-a11y/static/css/main.276df416.chunk.css"
+  },
+  {
+    "revision": "764c42f92e03088f9c72",
+    "url": "/post-process-a11y/static/js/2.7a194363.chunk.js"
+  },
+  {
+    "revision": "559fc2a4916b004906bb184e2b774ce1",
+    "url": "/post-process-a11y/static/js/2.7a194363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e76ae9ca86acbe23ec0",
+    "url": "/post-process-a11y/static/js/main.c6aba76a.chunk.js"
+  },
+  {
+    "revision": "bf3c7cd14d746db81353",
+    "url": "/post-process-a11y/static/js/runtime-main.4a22357e.js"
+  }
+]);
